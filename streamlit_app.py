eca66529f2222d63c298b7ffad210b28e590329f
profcone <148368093+profcone@users.noreply.github.com>
2026-09-21 22:20:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ session = cnx.session()
 
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options")\
-        .select( col("FRUIT_NAME") )
+        .select( col("FRUIT_NAME"), col("SEARCH_ON") )
 st.dataframe( data = my_dataframe,
               use_container_width = True )
 st.stop()
