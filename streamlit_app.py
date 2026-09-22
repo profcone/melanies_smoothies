ce98677eacdf262d39932e19ba5a4bf5676e6338
profcone <148368093+profcone@users.noreply.github.com>
2026-09-21 21:51:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ from snowflake.snowpark.functions import col
 import os
 import requests  
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-
-st.text(smoothiefroot_response)
-
 # Write directly to the app
 st.title('My Parents New Healthy Diner')
 st.write(
@@ -65,5 +61,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, {}!'\
             .format(name_on_order), icon="✅")
+      
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+
+st.text(smoothiefroot_response)
 
     
